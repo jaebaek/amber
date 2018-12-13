bc3dfd1f4e30dfe2a4dc58fe80f2aa9b5fa20f7c
Jaebaek Seo <jaebaek@google.com>
2018-12-13 14:22:47 -0500

    Vulkan: fix validation errors
    Fixes #185

--- src/vulkan/buffer_descriptor.cc
+++ src/vulkan/buffer_descriptor.cc
@@ -121,6 +121,8 @@ Result BufferDescriptor::CreateOrResizeIfNeeded(
 
     new_buffer->CopyFromBuffer(command, *buffer_);
 
+    not_destroyed_buffers_.push_back(std::move(buffer_));
+
     buffer_ = std::move(new_buffer);
 
     SetUpdateDescriptorSetNeeded();
@@ -171,6 +173,8 @@ ResourceInfo BufferDescriptor::GetResourceInfo() {
 
 void BufferDescriptor::Shutdown() {
   buffer_->Shutdown();
+  for (auto& buffer : not_destroyed_buffers_)
+    buffer->Shutdown();
 }
 
 }  // namespace vulkan
